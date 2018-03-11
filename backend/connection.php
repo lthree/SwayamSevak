ab44dfeac6fc90c00217da7f443734c5385bb87f
Shaleen Jain <anuj1008@gmail.com>
2018-03-11 11:55:49 +0530

    Update connection.php

--- backend/connection.php
+++ backend/connection.php
@@ -1,14 +1,28 @@
-<?php
-$host="127.0.0.1";
-$port=3306;
-$socket="";
-$user="mumbaihack";
-$password="mumbaihack";
-$dbname="shaleentestdb";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-if($con){
-//echo "Connected";
-}
-?>
+<?php
+
+$host="127.0.0.1";
+
+$port=3306;
+
+$socket="";
+
+$user="******";
+
+$password="******";
+
+$dbname="********";
+
+
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+if($con){
+
+//echo "Connected";
+
+}
+
+?>
+
