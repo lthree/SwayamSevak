59592f22cf411b4831e93bd1a4058d02c5536f1e
pranavkantgaur <pranav8iisc@gmail.com>
2018-05-31 13:59:34 +0530

    fixes compile errors in update participant details

--- frontend/app/src/main/java/com/example/pranav/swayamsevakclient/UpdateParticipantDetailsActivity.java
+++ frontend/app/src/main/java/com/example/pranav/swayamsevakclient/UpdateParticipantDetailsActivity.java
@@ -26,6 +26,7 @@ import java.util.Map;
 public class UpdateParticipantDetailsActivity extends AppCompatActivity {
 
     String mParticipantName, mParticipantAddress, mParticipantEmailAddress, mParticipantContactNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,57 +46,59 @@ public class UpdateParticipantDetailsActivity extends AppCompatActivity {
         // send that information to DB on Submit click button
         Button submit_participant_info_button = (Button) findViewById(R.id.submit_participant_details_button);
         submit_participant_info_button.setOnClickListener(new View.OnClickListener() {
-                                                        @Override
-                                                        public void onClick(View view) {
-                                                            // send that information to DB
-                                                            JSONObject updateParticipantDetails = new JSONObject();
-                                                            try{
-                                                                updateParticipantDetails.put("name", mParticipantName);
-                                                                updateParticipantDetails.put("address", mParticipantAddress);
-                                                                updateParticipantDetails.put("emailid", mParticipantEmailAddress);
-                                                                updateParticipantDetails.put("contactnumber", mParticipantContactNumber);
-                                                                JsonObjectRequest sendParticipantUpdatedDataRequest = new JsonObjectRequest(Request.Method.POST, AppConfig.URL_UPDATE_PARTICIPANT_DETAILS, updateParticipantDetails, new Response.Listener<JSONObject>() {
-                                                                    @Override
-                                                                    public void onResponse(JSONObject response) {
-                                                                        Log.d(TAG, response.toString());
-                                                                        Toast.makeText(UpdateParticipantDetailsActivity.this, "Details successfully updated!!", Toast.LENGTH_SHORT).show();
+                                                              @Override
+                                                              public void onClick(View view) {
+                                                                  // send that information to DB
+                                                                  JSONObject updateParticipantDetails = new JSONObject();
+                                                                  try {
+                                                                      updateParticipantDetails.put("name", mParticipantName);
+                                                                      updateParticipantDetails.put("address", mParticipantAddress);
+                                                                      updateParticipantDetails.put("emailid", mParticipantEmailAddress);
+                                                                      updateParticipantDetails.put("contactnumber", mParticipantContactNumber);
+                                                                      JsonObjectRequest sendParticipantUpdatedDataRequest = new JsonObjectRequest(Request.Method.POST, AppConfig.URL_UPDATE_PARTICIPANT_DETAILS, updateParticipantDetails, new Response.Listener<JSONObject>() {
+                                                                          @Override
+                                                                          public void onResponse(JSONObject response) {
+                                                                              Log.d("TAG", response.toString());
+                                                                              Toast.makeText(UpdateParticipantDetailsActivity.this, "Details successfully updated!!", Toast.LENGTH_SHORT).show();
 
-                                                                        // invoke status activity
-                                                                        Intent participant_details_updated_info_intent = new Intent(getApplicationContext(), ParticipantDetailsUpdatedActivity.class);
-                                                                        startActivity(participant_details_updated_info_intent);
+                                                                              // invoke status activity
+                                                                              Toast.makeText(UpdateParticipantDetailsActivity.this, "Details updated successfully !!", Toast.LENGTH_SHORT).show();
 
-                                                                    }
-                                                                }, new Response.ErrorListener() {
-                                                                    @Override
-                                                                    public void onErrorResponse(VolleyError error) {
-                                                                        VolleyLog.d(TAG, "Error: " + error.getMessage());
-                                                                    }
-                                                                }){
+                                                                          }
+                                                                      }, new Response.ErrorListener() {
+                                                                          @Override
+                                                                          public void onErrorResponse(VolleyError error) {
+                                                                              VolleyLog.d("TAG", "Error: " + error.getMessage());
+                                                                          }
+                                                                      }) {
+                                                                          @Override
+                                                                          public Map<String, String> getParams() {
+                                                                              SessionManager sessionManager = new SessionManager(getApplicationContext());
+                                                                              Map<String, String> params = new HashMap<String, String>();
+                                                                              params.put("loginToken", sessionManager.getLoginToken());
+                                                                              return params;
+                                                                          }
 
-                                                                    @Override
-                                                                    public Map<String, String> getParams(){
-                                                                        SessionManager sessionManager = new SessionManager(getApplicationContext());
-                                                                        Map<String, String> params  = new HashMap<String, String>();
-                                                                        params.put("loginToken", sessionManager.getLoginToken());
-                                                                        return params;
-                                                                    }
-
-                                                                    @Override
-                                                                    public Map<String, String> getHeaders() throws AuthFailureError {
-                                                                        Map<String,String> params = new HashMap<String, String>();
-                                                                        params.put("Content-Type","application/x-www-form-urlencoded");
-                                                                        return params;
-                                                                    };                                                                    ;
-
-
-                                                                AppController.getInstance().addToRequestQueue(sendParticipantUpdatedDataRequest); // adds the request to the queue
-                                                            }
-                                                            catch (JSONException e){
-                                                                e.printStackTrace();
-                                                            }
+                                                                          @Override
+                                                                          public Map<String, String> getHeaders() throws AuthFailureError {
+                                                                              Map<String, String> params = new HashMap<String, String>();
+                                                                              params.put("Content-Type", "application/x-www-form-urlencoded");
+                                                                              return params;
+                                                                          }
+                                                                      };
+                                                                      ;
 
 
-                                                        }
-                                                    }
-    }
-}
+                                                                      AppController.getInstance().addToRequestQueue(sendParticipantUpdatedDataRequest); // adds the request to the queue
+                                                                  } catch (JSONException e) {
+                                                                      e.printStackTrace();
+                                                                  }
+
+                                                              }
+                                                          }
+
+        );
+
+    }}
+
+
