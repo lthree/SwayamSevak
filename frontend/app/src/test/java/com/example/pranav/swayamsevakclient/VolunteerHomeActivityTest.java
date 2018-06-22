0b16ccfc636ef2e79b547f9acc71ca1fe173a6ca
safiaw <wahdat.safia@gmail.com>
2018-06-22 15:07:21 +0530

    Added one Unit testing class

---
+++ frontend/app/src/test/java/com/example/pranav/swayamsevakclient/VolunteerHomeActivityTest.java
@@ -0,0 +1,26 @@
+package com.example.pranav.swayamsevakclient;
+
+import android.test.suitebuilder.annotation.MediumTest;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+/**
+ * Created by Safia on 22-06-2018.
+ */
+@MediumTest
+//@RunWith(AndroidJUnit4.class) // annotating test classes with AndroidJUnit4.class, use it to run the tests
+public class VolunteerHomeActivityTest {
+
+
+}
+
